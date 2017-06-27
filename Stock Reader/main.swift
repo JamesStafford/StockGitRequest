135386bf3198c3457c81375b6065df6aecbdbdad
James Stafford <jstafford@bomgar.com>
2017-06-26 20:35:02 -0500

    Initial Commit

---
+++ Stock Reader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Stock Reader
+//
+//  Created by James Stafford on 6/26/17.
+//  Copyright © 2017 James Stafford. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
