8c72e58c3a2d5cd1de486e5ee8b771274ecd1acc
James Stafford <jstafford@bomgar.com>
2017-06-26 21:26:35 -0500

    Successfully created a GET request to Quandl NASDAQ OMX Global Index Data.

--- Stock Reader/main.swift
+++ Stock Reader/main.swift
@@ -1,6 +1,8 @@
 //
 //  main.swift
 //  Stock Reader
+//  
+//  Learning how to make HTTP requests.
 //
 //  Created by James Stafford on 6/26/17.
 //  Copyright © 2017 James Stafford. All rights reserved.
@@ -8,5 +10,22 @@
 
 import Foundation
 
-print("Hello, World!")
+let key = String()
+let url = URL(string: "https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=\(key)")!
+let urlSession = URLSession(configuration: .default)
 
+let task = urlSession.dataTask(with: url) {
+    (data, response, error) in
+    if (error != nil) {
+        print("error: \(String(describing: error))")
+    } else {
+        print("data: \(String(describing: data))")
+        print("response: \(String(describing: response))")
+        
+        let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+        print("json: \(String(describing: json))")
+    }
+}
+task.resume()
+
+RunLoop.main.run()
